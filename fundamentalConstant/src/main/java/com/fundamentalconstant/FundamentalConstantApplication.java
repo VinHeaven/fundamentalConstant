d83d96ab3f4691bac8e7f5157900395928248964
FloydDanton <floyd-danton@web.de>
2021-06-11 17:07:45 +0200

    initial

---
+++ fundamentalConstant/src/main/java/com/fundamentalconstant/FundamentalConstantApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentalconstant;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class FundamentalConstantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalConstantApplication.class, args);
+    }
+
+}
---
+++ fundamentalConstant/src/test/java/com/fundamentalconstant/FundamentalConstantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentalconstant;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class FundamentalConstantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
